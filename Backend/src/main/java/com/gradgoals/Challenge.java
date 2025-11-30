9824905321e084426057be6aa5caf5f9e78837bb
murphyz21 <143122177+murphyz21@users.noreply.github.com>
2025-11-30 13:00:31 -0700

    Reorganizing

---
+++ Backend/src/main/java/com/gradgoals/Challenge.java
@@ -0,0 +1,17 @@
+public class Challenge {
+    private String answer;
+
+    public Challenge() {}
+
+    public Challenge(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
---
+++ Backend/src/main/java/com/gradgoals/ChallengeResponse.java
@@ -0,0 +1,17 @@
+public class ChallengeResponse {
+    private boolean correct;
+    private String message;
+
+    public ChallengeResponse(boolean correct, String message) {
+        this.correct = correct;
+        this.message = message;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
