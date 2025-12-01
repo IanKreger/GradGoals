5b3bcc4a3e48ccf2a4d4d28187fe5b87acd33495
katsullivan58-rgb <katsullivan58@gmail.com>
2025-12-01 14:11:13 -0600

    Fix formatting issue in menuItems array

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -31,7 +31,7 @@ const menuItems = [
   { name: 'Home', link: 'Index.html' },
   { name: 'Resources', link: 'Resources.html' },
   { name: 'Budget Tool', link: 'Budget.html' },
-  { name: 'Challenges', link: 'Challenge.html' }
+  { name: 'Challenges', link: 'Challenge.html'}
 ];
 
 // Build nav items
